d6da7080e96c3f68af1cfdf26c1c83ede1f2927a
Filip Danić <filip@spicefactory.co>
2018-07-26 10:46:55 +0200

    switch to es5 syntax

--- index.js
+++ index.js
@@ -1,77 +1,27 @@
-/**
- * functional-match-case
- * --------------
- * `npm install functional-match-case`
- *  or
- * `yarn add functional-match-case`
- *
- * Example:
- *
- * Turn:
- *
- * switch(someValue) {
- *  case A:
- *  case B:
- *    return resultA;
- *  case C:
- *    return resultB;
- *  case D:
- *    return functionC();
- *  default:
- *    return defaultValue;
- * }
- *
- * Into:
- *
- * import matchCase from 'functional-match-case';
- *
- * const match = matchCase({
- *   [A]: resultA,
- *   [B]: resultA,
- *   [C]: resultB,
- *   [D]: functionC, // just the ref, will be called when needed
- * })(defaultValue);
- *
- * Then whenever needed:
- *
- * match(someValue);
- *
- * Extra benefits:
- *
- * Comparability of cases. For example:
- *
- * someMatchCase = {
- *   [A]: resultA,
- *   [B]: resultB,
- * };
- *
- * anotherMatchCase = {
- *   [C]: resultC,
- *   [D]: functionD,
- * }
- *
- * composedMatcher = matchCase({
- *    ...someMatchCase,
- *    ...anotherMatchCase,
- *    [F]: resultF,
- * })(defaultValue);
- *
- */
-
 const executeIfFunction = require('execute-if-function');
 
 /**
  * @param {Object} cases
  * @return {function(*): function(string|number=): *}
  */
-const switchCaseHashMap = cases => defaultCase => key =>
-  cases.hasOwnProperty(key) ? cases[key] : defaultCase;
+var switchCaseHashMap = function switchCaseHashMap(cases) {
+  return function (defaultCase) {
+    return function (key) {
+      return cases.hasOwnProperty(key) ? cases[key] : defaultCase;
+    };
+  };
+};
 
 /**
  * @param {Object} cases
  * @return {function(*=): function(string|number=): *}
  */
-const matchCase = cases => defaultCase => key =>
-  executeIfFunction(switchCaseHashMap(cases)(defaultCase)(key));
+var matchCase = function matchCase(cases) {
+  return function (defaultCase) {
+    return function (key) {
+      return executeIfFunction(switchCaseHashMap(cases)(defaultCase)(key));
+    };
+  };
+};
 
 module.exports = matchCase;
